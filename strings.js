9e0c64c8e472b5289a0bf82fd8a6a8fd113ef46f
Faith-Omenta <faithbomenta@gmail.com>
2023-03-10 23:44:43 +0300

    fifth grade

---
+++ strings.js
@@ -0,0 +1,30 @@
+let x ="beautiful"
+console.log(x[0],x[1],x[2],x[3])
+
+const t = "eat"
+const food="I did not have appetite today"
+console.log(food.replace(food[0],"eat"))
+
+const story="She sells sea shells at the sea shore"
+const char="the"
+const count=strory.split(char).length
+console.log(count)
+const char1="t"
+const count1=story.split(char).lengthconsole.log(count1);
+
+const house="CONFident";
+const high=capital.toUpperCase();
+console.log(capital);
+const woman="amazing"
+const ladys=less.toLowerCase();
+console.log(smaller);
+const home="A busy office";
+const home1=tittle.toLocalUpperCase();
+console.log(home1)
+
+const string1="The lady went to the market with her sister";
+const sentence=string1.search(market);
+console.log(shop);
+const string2="My favorite season is spring";
+const girl=string2.search("season");
+console.log(person);
