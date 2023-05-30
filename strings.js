017bf50da90d4deee7349d002c5888eb98cda4e7
Faith-Omenta <faithbomenta@gmail.com>
2023-05-30 16:04:22 +0300

    commit

--- strings.js
+++ strings.js
@@ -1,30 +1,47 @@
+// Extract the first four characters from the string below;
 let x ="beautiful"
-console.log(x[0],x[1],x[2],x[3])
+let tex=x.slice(0,4)
+console.log(tex)
 
-const t = "eat"
+
+// Insert the following string at the tenth index of the below variable:
+// "eat"
+// const food = "I did not have appetite today"
 const food="I did not have appetite today"
-console.log(food.replace(food[0],"eat"))
+console.log=(food.replace(food[10],"eat"))
 
-const story="She sells sea shells at the sea shore"
-const char="the"
-const count=strory.split(char).length
-console.log(count)
-const char1="t"
-const count1=story.split(char).lengthconsole.log(count1);
 
+// Count how many times the following string appears in the string variable:
+// "the""
+// "s"
+//const story= "She sells sea shells at the sea shore"
+const text="She sells sea shells at the sea shore"
+const f=(text.match(/the/g)).length;
+const t=(text.match(/s/g)).length;
+console.log(f);
+console.log(t);
+// convert the following strings into the specified format:
+// 1. UpperCase: "CONfidant"
+// 2. LowerCase: "amazing", "beautiFUL"
+// 3. Title case "A busy office"
 const house="CONFident";
-const high=capital.toUpperCase();
-console.log(capital);
+let high=house.toUpperCase();
+console.log(high);
 const woman="amazing"
-const ladys=less.toLowerCase();
-console.log(smaller);
+let ladys=woman.toLowerCase();
+console.log(ladys);
 const home="A busy office";
-const home1=tittle.toLocalUpperCase();
+let home1=home.toLocaleUpperCase();
 console.log(home1)
 
+//Using JavaScript, find the following words from the following strings:
+ // 1."market"
+// const string1 = "The lady went to the market with her sister";
+// 2. "season"
+// const string2 = "My favorite season is spring"
 const string1="The lady went to the market with her sister";
-const sentence=string1.search(market);
-console.log(shop);
+let sentence=string1.search("market");
+console.log("sentence");
 const string2="My favorite season is spring";
-const girl=string2.search("season");
-console.log(person);
+let girl=string2.search("season");
+console.log(girl);
